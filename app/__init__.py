cdae11e89489b09e4d98171dedbb3f5d9aee9da7
sanjog <sanjogsinghbhatia2003@gmail.com>
2026-09-03 17:05:34 +0530

    resolved CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 import turso_serverless
 
 
 app = application = Flask(__name__)
 app.config.from_object('config')
+
+CORS(app, origins=[
+    "http://localhost:5173", "https://ai-rag-two.vercel.app"
+])
 
 def turso_conn():
     conn = turso_serverless.connect(
